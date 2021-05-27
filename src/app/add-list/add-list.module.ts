a760a89578eb3d9f80477ad039a966efad9f237d
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-05-27 14:51:30 -0400

    start forms for making Lists

--- src/app/add-list/add-list.module.ts
+++ src/app/add-list/add-list.module.ts
@@ -4,6 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AddListRoutingModule } from './add-list-routing.module';
 import { AddListComponent } from './add-list/add-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -13,7 +21,16 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AddListRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    DragDropModule
     
   ]
 })
--- src/app/add-list/add-list/add-list.component.ts
+++ src/app/add-list/add-list/add-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Card, SetExpansion } from 'src/app/_objects/expansion';
+import { CollectionService } from 'src/app/_services/collection.service';
 
 @Component({
   selector: 'app-add-list',
@@ -9,14 +13,52 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class AddListComponent implements OnInit {
 
   listForm: FormGroup;
+  cardForm: FormGroup;
+  listTypes = ['checklist', 'count list'];
+  cards: string[] = [];
 
-  constructor(private fb: FormBuilder) { }
+  expansions: {};
+  expansionNames: string[];
+
+  constructor(
+    private fb: FormBuilder,
+    private collectionserv: CollectionService,
+    private dialogRef: MatDialogRef<AddListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    this.expansions = this.collectionserv.expansions.value;
+    this.expansionNames = Object.keys(this.expansions);
+    this.listForm = this.createListForm();
+    this.cardForm = this.createCardForm();
   }
 
-  createForm(): FormGroup {
-    return this.fb.group({});
+  createListForm(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      listType: 'checklist',
+    });
+  }
+
+  createCardForm(): FormGroup {
+    return this.fb.group({
+      expansion: this.expansionNames[0],
+      print: [1, Validators.required],
+    })
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  // Drag and drop
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  }
+
+  addCard(): void {
+    const cardinfo = this.cardForm.value;
+    this.cards.push(`${cardinfo.expansion}-${cardinfo.print}`);
   }
 
 }
