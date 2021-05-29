e3842589adc2a2719a0663d94a04826a3c52d77a
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-05-28 20:50:47 -0400

    show all placeholder info

--- src/app/card-preview/individual-card/individual-card.component.ts
+++ src/app/card-preview/individual-card/individual-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddCardComponent } from 'src/app/add-card/add-card/add-card.component';
 import { CardInstance } from 'src/app/_objects/card-instance';
+import { Card, SetExpansion } from 'src/app/_objects/expansion';
+import { CollectionService } from 'src/app/_services/collection.service';
 
 @Component({
   selector: 'app-individual-card',
@@ -12,10 +14,27 @@ export class IndividualCardComponent implements OnInit {
 
   @Input() instance: CardInstance = new CardInstance(0, 'False Hoods', 'Spectacular', 'M', []);
   @Input() allowEdit: boolean = false;
+  @Input() mainKey: string = '';
 
-  constructor(private dialog: MatDialog) { }
+  exp: SetExpansion;
+  cardType: Card;
 
-  ngOnInit(): void {
+
+  constructor(
+    private collectionserv: CollectionService,
+    private dialog: MatDialog) { }
+
+  ngOnInit(): void { 
+    this.exp = this.collectionserv.expansions.value[this.instance.expansionName];
+    this.cardType = this.exp.cards[this.instance.printNumber-1];
+  }
+
+  isPlaceholder() {
+    const cardKey = `${this.instance.expansionName}-${this.instance.printNumber}`;
+    if (this.mainKey && this.mainKey !== cardKey) {
+      return true;
+    }
+    return false;
   }
 
   edit(): void {
