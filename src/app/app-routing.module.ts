50bc5f0428d1cbaaf34762c250073ffd2eff4d2d
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-05-31 06:59:19 -0400

    untangle routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '',
+    loadChildren: () => import('./display-lists/display-lists.module')
+    .then(m => m.DisplayListsModule)
+  },
   { path: 'expansion',
     loadChildren: () => import('./add-expansion/add-expansion.module')
       .then(m => m.AddExpansionModule)
+  },
+  { path: 'card',
+    loadChildren: () => import('./add-card/add-card.module')
+      .then(m => m.AddCardModule)
   },
   { path: 'checklist',
     loadChildren: () => import('./add-list/add-list.module')
       .then(m => m.AddListModule)
   },
-  { path: 'card',
-    loadChildren: () => import('./add-card/add-card.module')
-      .then(m => m.AddCardModule)
-  }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
