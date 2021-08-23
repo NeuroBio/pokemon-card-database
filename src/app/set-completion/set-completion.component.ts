06f0dac61d7a7baf93a0310af100469f19dd4ab3
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-23 11:46:27 -0400

    add more info to stats

--- src/app/set-completion/set-completion.component.ts
+++ src/app/set-completion/set-completion.component.ts
@@ -38,29 +38,57 @@ export class SetCompletionComponent implements OnInit {
     // create set data if it does not exist
     if (!this.setInfo[expName]) {
       const exp = this.expansions[expName];
-      const rarity = {};
+      const rarityOwned = {};
+      const rarityOwnedPokemon = {};
+      const rarityTotal = {};
+      const rarityTotalPokemon = {};
       const ownership = [];
       let cardCount = 0;
       let unique = 0;
+      let uniquePokemon = 0;
 
       exp.cards.forEach(card => {
+        const activeCard = exp.cards[card.printNumber - 1];
+
+        // get stats for all cards in set by rarity
+        if (!rarityTotal[activeCard.rarity]) {
+          rarityTotal[activeCard.rarity] = 0;
+          rarityTotalPokemon[card.rarity] = 0;
+        }
+        rarityTotal[activeCard.rarity] += 1;
+        if (activeCard.cardType === 'Pokémon') {
+          rarityTotalPokemon[card.rarity] += 1;
+        }
+
+        // get stats based on card instances
         const instances = this.cards[`${expName}-${card.printNumber}`];
         if (instances) {
           const numCards = Object.keys(instances).length;
           unique += 1;
+          if (activeCard.cardType === 'Pokémon' && activeCard.rarity !== 'Secret Rare') {
+            uniquePokemon += 1
+          }
           cardCount += numCards;
           ownership.push(numCards);
-          if (!rarity[card.rarity]) {
-            rarity[card.rarity] = 0;
+          if (!rarityOwned[card.rarity]) {
+            rarityOwned[card.rarity] = 0;
+            rarityOwnedPokemon[card.rarity] = 0;
           }
-          rarity[card.rarity] += 1;
+          rarityOwned[card.rarity] += 1;
+          if (activeCard.cardType === 'Pokémon' && activeCard.rarity !== 'Secret Rare') {
+            rarityOwnedPokemon[card.rarity] += 1;
+          }
         } else {
           ownership.push(0);
         }
       });
 
-      this.setInfo[expName] =  { expName, rarity, cardCount,
-        unique, ownership, print: exp.numCards, total: exp.cards.length };
+      this.setInfo[expName] =  { expName, rarityOwned, rarityOwnedPokemon,
+        rarityTotal, rarityTotalPokemon,
+        cardCount, unique, ownership, print: exp.numCards,
+        total: exp.cards.length, totalPokemon: uniquePokemon,
+        ownedPokemon: exp.cards.filter(card => card.cardType === 'Pokémon').length
+      };
     }
 
     // set the active data
@@ -70,6 +98,18 @@ export class SetCompletionComponent implements OnInit {
   getCardDetails(print: number): void {
     const card = this.expansions[this.activeSet.value].cards[print];
     this.checkCard = `${card.cardTitle}-${card.rarity}`;
+  }
+
+  getCompletionPercent(have: number, total: number): number {
+    if (!have) {
+      return 0;
+    }
+    return Math.round(have / total * 10000) / 100;
+  }
+
+  isPokemon(index: number): boolean {
+    const card = this.expansions[this.activeSet.value].cards[index];
+    return (card.cardType === 'Pokémon' && card.rarity !== 'Secret Rare');
   }
 
 }
